ca8583927864edf58224b2aeed7f0f950cbd0982
Manuel Araujo <manuelmad1981@gmail.com>
2024-03-28 09:07:16 -0400

    Adding AppCheck to secure my Firebase's Credentials.

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
-
-//   import { initializeApp } from '../node_modules/firebase/app';
-//   import { getFirestore } from '../node_modules/firebase/app';
-
+import { initializeAppCheck, ReCaptchaEnterpriseProvider } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app-check.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
-
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js"
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -20,6 +16,26 @@ appId: "1:895345772809:web:84bedc4b4c6c051db7ebd8"
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+
+// Getting Token to use the app in local environment (only in my laptop)
+// self.FIREBASE_APPCHECK_DEBUG_TOKEN = true; // I get the token at Browser's Console, copy it and save it in AppCheck at Firebase Website, I comment this line so it's not visible anymore at Browser's Console.
+
+// Create a ReCaptchaEnterpriseProvider instance using your reCAPTCHA Enterprise site key and pass it to initializeAppCheck().
+const appCheck = initializeAppCheck(app, {
+    provider: new ReCaptchaEnterpriseProvider("6Ld8vaQpAAAAAOmBQPg2yw3XsdN5OuQ7zJd-xaYr"),
+    isTokenAutoRefreshEnabled: true // Set to true to allow auto-refresh.
+});
+
+function onClick(e) {
+    e.preventDefault();
+    grecaptcha.enterprise.ready(async () => {
+      const token = await grecaptcha.enterprise.execute('6Ld8vaQpAAAAAOmBQPg2yw3XsdN5OuQ7zJd-xaYr', {action: 'LOGIN'});
+      // IMPORTANT: The 'token' that results from execute is an encrypted response sent by
+      // reCAPTCHA Enterprise to the end user's browser.
+      // This token must be validated by creating an assessment.
+      // See https://cloud.google.com/recaptcha-enterprise/docs/create-assessment
+    });
+}
 
 export const auth = getAuth(app);
 
